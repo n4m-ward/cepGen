f4c70f483c18b614d8bb7d72d2201650fedf5d7f
jorgelima <jorge.lima@teknisa.com>
2020-10-21 15:00:15 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+ use Nam\DigitalCep\Search;
+ require_once "vendor/autoload.php";
+
+ $busca = new Search;
+ 
+ $resultado = $busca->getAddressFromZipcode('15155000');
+ 
+ print_r($resultado);
